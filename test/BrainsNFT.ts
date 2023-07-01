b429d8d5bcf74084c36bd14e2a2f8dae6c99a0fa
Dmytro Morozov <dmytro@codefunded.com>
2023-07-02 00:11:32 +0200

    Added unit tests for Mint, Burn and Transfer

--- test/BrainsNFT.ts
+++ test/BrainsNFT.ts
@@ -6,12 +6,16 @@ import { BigNumber } from "ethers";
 
 describe("BrainsNFT", function () {
   async function deploy() {
-    const [owner] = await ethers.getSigners();
+    const [owner, otherAccount] = await ethers.getSigners();
 
     const BrainsNFT = await ethers.getContractFactory("BrainsNFT");
     const contract = await BrainsNFT.deploy();
 
-    return { contract, owner };
+    const tx = await contract.safeMint(owner.address, 'reward.png', 12);
+    const receipt = await tx.wait();
+    const tokenId: BigNumber = (receipt.events?.[0] as any).args.tokenId;
+
+    return { contract, owner, otherAccount, tokenId };
   }
 
   describe("Deployment", function () {
@@ -37,34 +41,106 @@ describe("BrainsNFT", function () {
   describe("Mint", function () {
 
     it("Should mint a token with proper owner", async function () {
-        const { contract, owner } = await loadFixture(deploy);
-       
-        const tx = await contract.safeMint(owner.address, 'reward.png', 12);
-        const receipt = await tx.wait();
-        const tokenId: BigNumber = (receipt.events?.[0] as any).args.tokenId;
+        const { contract, owner, tokenId } = await loadFixture(deploy);
 
         expect(await contract.ownerOf(tokenId)).to.equal(owner.address);
+        expect(await contract.balanceOf(owner.address)).to.equal(1);
     });
 
     it("Should mint a token with proper URI", async function () {
-        const { contract, owner } = await loadFixture(deploy);
+        const { contract, tokenId } = await loadFixture(deploy);
        
-        const tx = await contract.safeMint(owner.address, 'reward.png', 12);
-        const receipt = await tx.wait();
-        const tokenId: BigNumber = (receipt.events?.[0] as any).args.tokenId;
-
         expect(await contract.tokenURI(tokenId)).to.equal('reward.png');
     });
 
     it("Should mint a token with proper reward", async function () {
-        const { contract, owner } = await loadFixture(deploy);
-       
-        const tx = await contract.safeMint(owner.address, 'reward.png', 12);
-        const receipt = await tx.wait();
-        const tokenId: BigNumber = (receipt.events?.[0] as any).args.tokenId;
+        const { contract, tokenId } = await loadFixture(deploy);
 
         expect(await contract.tokenValue(tokenId)).to.equal(12);
     });
 
-  })
+    it("Should mint a token incremented id", async function () {
+        const { contract, owner, tokenId } = await loadFixture(deploy);
+
+        const tx = await contract.safeMint(owner.address, 'reward.png', 12);
+        const receipt = await tx.wait();
+        const anotherTokenId: BigNumber = (receipt.events?.[0] as any).args.tokenId;
+
+        expect(anotherTokenId).to.be.greaterThan(tokenId);
+    });
+
+    it("Should fail if access value of non-existing token", async function () {
+        const { contract } = await loadFixture(deploy);
+
+        await expect(contract.tokenValue(123)).to.be.revertedWith("ERC721: invalid token ID");
+    });
+
+    it("Should fail if access uri of non-existing token", async function () {
+        const { contract } = await loadFixture(deploy);
+
+        await expect(contract.tokenURI(123)).to.be.revertedWith("ERC721: invalid token ID");
+    });
+  });
+
+    describe("Transfer", function () {
+        
+        it("Should transfer a token with proper owner", async function () {
+            const { contract, owner, tokenId, otherAccount } = await loadFixture(deploy);
+
+            await contract.transferFrom(owner.address, otherAccount.address, tokenId);
+
+            expect(await contract.ownerOf(tokenId)).to.equal(otherAccount.address);
+            expect(await contract.balanceOf(otherAccount.address)).to.equal(1);
+        });
+
+        it("Should not transfer a token if not an owner", async function () {
+            const { contract, owner, tokenId, otherAccount } = await loadFixture(deploy);
+           
+            await expect(contract.transferFrom(otherAccount.address, owner.address, tokenId)).to.be.revertedWith("ERC721: transfer from incorrect owner");
+        });
+
+        it("Should transfer token if approved", async function () {
+            const { contract, owner, tokenId, otherAccount } = await loadFixture(deploy);
+
+            await contract.approve(otherAccount.address, tokenId);
+            await contract.connect(otherAccount).transferFrom(owner.address, otherAccount.address, tokenId, { from: otherAccount.address });
+
+            expect(await contract.ownerOf(tokenId)).to.equal(otherAccount.address);
+            expect(await contract.balanceOf(otherAccount.address)).to.equal(1);
+        });
+    });
+
+    describe("Burn", function () {
+            
+        it("Should burn a token", async function () {
+            const { contract, owner, tokenId } = await loadFixture(deploy);
+
+            await contract.burn(tokenId);
+
+            await expect(contract.ownerOf(tokenId)).to.be.revertedWith("ERC721: invalid token ID");
+            expect(await contract.balanceOf(owner.address)).to.equal(0);
+        });
+
+        it("Should not burn a token if not an owner", async function () {
+            const { contract, owner, tokenId, otherAccount } = await loadFixture(deploy);
+        
+            await expect(contract.connect(otherAccount).burn(tokenId)).to.be.revertedWith("ERC721: caller is not token owner or approved");
+        });
+
+        it("Should burn token if approved", async function () {
+            const { contract, owner, tokenId, otherAccount } = await loadFixture(deploy);
+
+            await contract.approve(otherAccount.address, tokenId);
+            await contract.connect(otherAccount).burn(tokenId);
+
+            await expect(contract.ownerOf(tokenId)).to.be.revertedWith("ERC721: invalid token ID");
+            expect(await contract.balanceOf(owner.address)).to.equal(0);
+        });
+
+        it("Should fail burn if token does not exist", async function () {
+            const { contract } = await loadFixture(deploy);
+
+            await expect(contract.burn(123)).to.be.revertedWith("ERC721: invalid token ID");
+        });
+    });
 });
