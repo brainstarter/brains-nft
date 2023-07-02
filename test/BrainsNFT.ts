a6d09c8c0fb50dfcec67aa1c80727e7445e2d4cc
Dmytro Morozov <dmytro@codefunded.com>
2023-07-02 13:27:00 +0200

    Added value, uri and pausable tests with erc20 mock

--- test/BrainsNFT.ts
+++ test/BrainsNFT.ts
@@ -11,11 +11,14 @@ describe("BrainsNFT", function () {
     const BrainsNFT = await ethers.getContractFactory("BrainsNFT");
     const contract = await BrainsNFT.deploy();
 
+    const ERC20Mock = await ethers.getContractFactory("ERC20Mock");
+    const erc20Contract = await ERC20Mock.deploy();
+
     const tx = await contract.safeMint(owner.address, 'reward.png', 12);
     const receipt = await tx.wait();
     const tokenId: BigNumber = (receipt.events?.[0] as any).args.tokenId;
 
-    return { contract, owner, otherAccount, tokenId };
+    return { contract, owner, otherAccount, tokenId, erc20Contract };
   }
 
   describe("Deployment", function () {
@@ -143,4 +146,119 @@ describe("BrainsNFT", function () {
             await expect(contract.burn(123)).to.be.revertedWith("ERC721: invalid token ID");
         });
     });
+
+    describe("Value", function () {
+
+        it("Should set token value if owner", async function () {
+            const { contract, tokenId } = await loadFixture(deploy);
+
+            const tx = await contract.setTokenValue(tokenId, 123);
+            await tx.wait();
+
+            expect(await contract.tokenValue(tokenId)).to.equal(123);
+        });
+
+        it("Should fail setting token value if not an owner", async function () {
+            const { contract, tokenId, otherAccount } = await loadFixture(deploy);
+
+            await expect(contract.connect(otherAccount).setTokenValue(tokenId, 123)).to.be.revertedWith("Ownable: caller is not the owner");
+        });
+    });
+
+    describe("Token URI", function () {
+
+        it("Should set token URI if owner", async function () {
+            const { contract, tokenId } = await loadFixture(deploy);
+
+            const tx = await contract.setTokenURI(tokenId, "newReard.png");
+            await tx.wait();
+
+            expect(await contract.tokenURI(tokenId)).to.equal("newReard.png");
+        });
+
+        it("Should fail setting token URI if not an owner", async function () {
+            const { contract, tokenId, otherAccount } = await loadFixture(deploy);
+
+            await expect(contract.connect(otherAccount).setTokenURI(tokenId, "newReard.png")).to.be.revertedWith("Ownable: caller is not the owner");
+        });
+    });
+
+    describe("Pausable", async function () {
+        it("should pause if owner", async function () {
+            const { contract } = await loadFixture(deploy);
+
+            await contract.pause();
+
+            expect(await contract.paused()).to.equal(true);
+        });
+
+        it("should unpause if owner", async function () {
+            const { contract } = await loadFixture(deploy);
+
+            await contract.pause();
+            await contract.unpause();
+
+            expect(await contract.paused()).to.equal(false);
+        });
+
+        it("should fail pause if already paused", async function () {
+            const { contract } = await loadFixture(deploy);
+
+            await contract.pause();
+
+            expect(await contract.paused()).to.equal(true);
+
+            await expect(contract.pause()).to.be.revertedWith("Pausable: paused");
+        });
+
+        it("should fail unpause if not paused", async function () {
+            const { contract } = await loadFixture(deploy);
+
+            expect(await contract.paused()).to.equal(false);
+
+            await expect(contract.unpause()).to.be.revertedWith("Pausable: not paused");
+        });
+
+        it("should fail pause if not owner", async function () {
+            const { contract, otherAccount } = await loadFixture(deploy);
+
+            await expect(contract.connect(otherAccount).pause()).to.be.revertedWith("Ownable: caller is not the owner");
+        });
+
+        it("should fail unpause if not owner", async function () {
+            const { contract, otherAccount } = await loadFixture(deploy);
+
+            await contract.pause();
+            expect(await contract.paused()).to.equal(true);
+
+            await expect(contract.connect(otherAccount).unpause()).to.be.revertedWith("Ownable: caller is not the owner");
+        });
+    })
+
+    // describe("Exchange", function () {
+
+    //     it("Should exchange tokens", async function () {
+    //         const { contract, owner, otherAccount, erc20Contract } = await loadFixture(deploy);
+
+    //         await erc20Contract.mint(owner.address, 1000);
+    //         await erc20Contract.transfer(contract.address, 1000);
+    //         console.log("ERC20 tokens minted and transfered");
+
+    //         const mintTx = await contract.safeMint(otherAccount.address, "reward1000.png", 1000);
+    //         const receipt = await mintTx.wait();
+    //         const tokenId: BigNumber = (receipt.events?.[0] as any).args.tokenId;
+    //         console.log("New NFT was minted");
+
+    //         await contract.enableExchange(erc20Contract.address);
+    //         console.log("Exchange enabled");
+
+    //         const tx = await contract.connect(otherAccount.address).exchange(tokenId);
+    //         await tx.wait();
+    //         console.log("Exchanged");
+
+    //         expect(await contract.ownerOf(tokenId)).to.equal(0);
+    //         expect(await erc20Contract.balanceOf(otherAccount.address)).to.equal(1000);
+    //     });
+
+    // });
 });
